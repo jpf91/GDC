283bc96efa8d72948b9104b522e255987e604d6b
Johannes Pfau <johannespfau@gmail.com>
2015-11-04 21:04:22 +0100

    Compile only x86_64 linux compilers

--- circle-ci.sh
+++ circle-ci.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 case $CIRCLE_NODE_INDEX in 0)
-       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.8/devkitPPC --toolchain=i686-w64-mingw32/gcc-4.8/devkitPPC --keep-alive=60
+       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.8/devkitPPC --keep-alive=60
    ;; 1) 
-       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.9/devkitARM --toolchain=i686-w64-mingw32/gcc-4.9/devkitARM --keep-alive=60
+       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.9/devkitARM --keep-alive=60
    ;; 2)
-       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=i686-linux-gnu/gcc-4.9/devkitARM --keep-alive=60
    ;; 3)
-       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=i686-linux-gnu/gcc-4.8/devkitPPC --keep-alive=60
 ;; esac
+
+#case $CIRCLE_NODE_INDEX in 0)
+#       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.8/devkitPPC --toolchain=i686-w64-mingw32/gcc-4.8/devkitPPC --keep-alive=60
+#   ;; 1) 
+#       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=x86_64-linux-gnu/gcc-4.9/devkitARM --toolchain=i686-w64-mingw32/gcc-4.9/devkitARM --keep-alive=60
+#   ;; 2)
+#       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=i686-linux-gnu/gcc-4.9/devkitARM --keep-alive=60
+#   ;; 3)
+#       docker run -v $HOME/docker-shared/:/home/build/shared jpf91/build-gdc /usr/bin/build-gdc build --toolchain=i686-linux-gnu/gcc-4.8/devkitPPC --keep-alive=60
+#;; esac
